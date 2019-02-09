9329593776151b2499313928be61af443b6cc815
adriamb <adria@codecontext.io>
2019-02-09 01:20:18 +0100

    optional itx

--- src/dbgapi.rs
+++ src/dbgapi.rs
@@ -40,7 +40,7 @@ pub struct DbgCallEntry {
 
 #[derive(Debug,Serialize,Deserialize)]
 pub struct DbgInternalTxs {
-    calls : Vec<DbgCallEntry>,
+    calls : Option<Vec<DbgCallEntry>>,
 }
 
 impl DbgInternalTxs {
@@ -56,16 +56,17 @@ impl DbgInternalTxs {
             } else {
                 Ok(Some(hex_to_addr(s)?))
             }
-        } 
-        for call in &self.calls {
-            info!("call.value=={}", call.value);
-            itxs.push(InternalTx{
-                from     : hex_to_addr(&call.from)?,
-                to       : opthex_to_addr(&call.to)?,
-                contract : opthex_to_addr(&call.to)?, // TODO: fix internal contract creation
-                input    : hex_to_vec(&call.input)?,
-                value    : if call.value == "0x0" { zero_u256 } else { hex_to_u256(&call.value)? }
-            })
+        }
+        if let Some(calls) = &self.calls { 
+            for call in calls {
+                itxs.push(InternalTx{
+                    from     : hex_to_addr(&call.from)?,
+                    to       : opthex_to_addr(&call.to)?,
+                    contract : opthex_to_addr(&call.to)?, // TODO: fix internal contract creation
+                    input    : hex_to_vec(&call.input)?,
+                    value    : if call.value == "0x0" { zero_u256 } else { hex_to_u256(&call.value)? }
+                })
+            }
         }
         Ok(itxs)
     }
@@ -73,14 +74,13 @@ impl DbgInternalTxs {
 
 impl<T: Transport> Dbg<T> {
     pub fn internal_txs(&self, tx: &Transaction) -> CallFuture<DbgInternalTxs, T::Out> {
-
-    CallFuture::new(
-        self.transport.execute  (
-            "debug_traceTransaction",
-            vec![
-                web3::helpers::serialize(&tx.hash),
-                serde_json::from_str("{\"tracer\":\"callTracer\"}").unwrap(),
-            ]
-    ))
-}
+        CallFuture::new(
+            self.transport.execute  (
+                "debug_traceTransaction",
+                vec![
+                    web3::helpers::serialize(&tx.hash),
+                    serde_json::from_str("{\"tracer\":\"callTracer\"}").unwrap(),
+                ]
+        ))
+    }
 }
